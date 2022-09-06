b1604bf3e83a42b5b6f2f27f25eb1a728c5d4715
Pddro1 <pedrohcamargon@gmail.com>
2022-09-06 11:14:58 -0300

    06/09

---
+++ lojadegames/src/main/java/com/generation/lojadegames/model/Produto.java
@@ -0,0 +1,70 @@
+package com.generation.lojadegames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String jogo;
+	
+	private String valor;
+	
+	private int lancamento;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getJogo() {
+		return this.jogo;
+	}
+	
+	public void setJogo(String jogo) {
+		this.jogo = jogo;
+	}
+	
+	public String getValor() {
+		return this.valor;
+	}
+	
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	
+	public int getLancamento() {
+		return this.lancamento;
+	}
+	
+	public void setLancamento(int lancamento) {
+		this.lancamento = lancamento;
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
